5a42a023342fa393a5e683c77ce741c8dfd7d6f7
dtsanko47 <daniel.tsanko@gmail.com>
2025-09-27 17:20:03 -0400

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,13 +1,13 @@
 #include "Node.h"
 
 
-Node::Node() : m_name(""), m_wins(0), m_losses(0), m_winDif(0), m_goals(0), m_lgoals(0), m_goalDif(0) {
+Node::Node() : m_name(""), m_wins(0), m_losses(0), m_winPrecentage(0), m_goals(0), m_goalsAgainst(0), m_goalDif(0) {
     cout << "No Team" << endl;
 }
 
-Node::Node(string name, int wins, int losses, double winDif, int goals, int lgoals, double goalDif):
-    m_name(name), m_wins(wins), m_losses(losses), m_winDif(winDif),
-    m_goals(goals), m_lgoals(lgoals), m_goalDif(goalDif) {}
+Node::Node(string name, int wins, int losses, double winDif, int goals, int goalsAgainst, int goalDif):
+    m_name(name), m_wins(wins), m_losses(losses), m_winPrecentage(winDif),
+    m_goals(goals), m_goalsAgainst(goalsAgainst), m_goalDif(goalDif) {}
 
 
 string Node::GetName() {
@@ -19,16 +19,16 @@ int Node::GetWins() {
 int Node::GetLosses() {
     return m_losses;
 }
-double Node::GetWinDif() {
-    return m_winDif;
+double Node::GetWinPrecentage() {
+    return m_winPrecentage;
 }
 int Node::GetGoals() {
     return m_goals;
 }
 int Node::GetLGoals() {
-    return m_lgoals;
+    return m_goalsAgainst;
 }
-double Node::GetGoalDif() {
+int Node::GetGoalDif() {
     return m_goalDif;
 }
 
@@ -36,12 +36,12 @@ Node* Node::GetNext() {
     return m_next;
 }
 
-void Node::SetTeam(int wins, int losses, double winDif, int goals, int lgoals, double goalDif){
+void Node::SetTeam(int wins, int losses, double winDif, int goals, int lgoals, int goalDif){
     m_wins = wins;
     m_losses = losses;
-    m_winDif = winDif;
+    m_winPrecentage = winDif;
     m_goals = goals;
-    m_lgoals = lgoals;
+    m_goalsAgainst = lgoals;
     m_goalDif = goalDif;
 }
 
