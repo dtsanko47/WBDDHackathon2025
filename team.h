faea9b3ec293e69a92ace5762159d5f05b363fa1
dtsanko47 <daniel.tsanko@gmail.com>
2025-09-27 14:12:36 -0400

    second commit

---
+++ team.h
@@ -0,0 +1,8 @@
+//
+// Created by danie on 9/27/2025.
+//
+
+#ifndef TEAM_H
+#define TEAM_H
+
+#endif //TEAM_H
---
+++ year.h
@@ -0,0 +1,11 @@
+//
+// Created by danie on 9/27/2025.
+//
+
+#ifndef YEAR_H
+#define YEAR_H
+#include <iostream>
+
+using namespace std;
+
+#endif //YEAR_H
