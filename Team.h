fa3b3d3144fd9947a497955824a4359b906bf4f6
aodogun1 <damola.odogun@gmail.com>
2025-09-27 14:23:25 -0400

    Create Team.h

---
+++ Team.h
@@ -0,0 +1,35 @@
+File Edit Options Buffers Tools C++ Help
+#ifndef TEAM_H
+#define TEAM_H
+
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+//Team object, stored within linked list
+struct Team{
+ public:
+
+  int m_wins;
+  int m_losses;
+  double m_winDif;
+  int m_goals;
+  int m_lgoals;
+  double m_goalDif;
+
+  Team(){}
+  Team(int wins, int losses, double winDif, int goals, int lgoals, double goalDif) {
+    m_wins = wins;
+    m_losses = losses;
+    m_winDif = winDif;
+    m_goals = goals;
+    m_lgoals = lgoals;
+    m_goalDif = goalDif;
+  }
+};
+
+#endif
+
+
